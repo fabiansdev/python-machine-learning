da027c789e97fc9a904e818a7dde1dbdb8d5ce42
fabiansdev <fabiansarmiento@protonmail.com>
2017-03-30 08:00:10 -0500

    More exercises, add scipy to requirements

--- MeanMedianMode.py
+++ MeanMedianMode.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
-
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 # crea un data set ficticio centrado alrededor de los 27000
 # con distribucion normal y desviacion estandar de 15000 con 10000 data points
 incomes =  np.random.normal(27000, 15000, 10000)
-print np.mean(incomes)
+print "Mean: ",np.mean(incomes)
+print "Median: ",np.median(incomes)
+
+
+# Moda: creamos un data set de 500 edades entre los 19 y 99 years
+ages = np.random.randint(18, 99, 500)
+print "Mode: ", stats.mode(ages)
 
 plt.hist(incomes, 50)
 plt.show()
-
